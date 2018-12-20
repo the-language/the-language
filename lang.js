3d12819c653449fab3055ade92f44d9a423ce9c7
zaoqi <zaomir@outlook.com>
2018-12-20 19:58:25 +0800

    .

--- lang.js
+++ lang.js
@@ -265,7 +265,7 @@ var TheLanguage=(function(){
     var the_sym=new_symbol("特一定其");
     var sth_sym=new_symbol("省略之其");
     var a_sym=new_symbol("一");
-    var pred_sym=new_symbol("邪乎否与");
+    var isornot_sym=new_symbol("邪乎否与");
     var true_sym=new_symbol("陽");
     var false_sym=new_symbol("陰");
     var bool_sym=new_symbol("陰陽");
@@ -278,9 +278,10 @@ var TheLanguage=(function(){
     var sym_sym=new_symbol("符");
 
     var builtin_func_new_cons_sym=new_data(sys_sym, new_list(name_sym, new_list(a_sym, new_list(func_sym, sth_sym, cons_sym), the_sym)));
-    var builtin_func_cons_p_sym=new_data(sys_sym, new_list(name_sym, new_list(WIP)));
+    var builtin_func_cons_p_sym=new_data(sys_sym, new_list(name_sym, new_list(a_sym, func_sym, new_list(isornot_sym, new_list(a_sym, cons_sym)))));
+    var builtin_func_cons_head_sym=new_data(sys_sym, new_list(name_sym, new_list(a_sym, new_list(func_sym, new_list(cons_sym), sth_sym), new_symbol("首"))));
     
-    var builtin_func_equal_sym=new_data(sys_sym, new_list(name_sym, new_list(a_sym, func_sym, new_list(pred_sym, equal_sym))));
+    var builtin_func_equal_sym=new_data(sys_sym, new_list(name_sym, new_list(a_sym, func_sym, new_list(isornot_sym, equal_sym))));
     var builtin_form_quote_sym=new_data(sys_sym, new_list(name_sym, new_list(a_sym, form_sym, quote_sym)));
     
     var builtin_func_apply_sym=new_data(sys_sym, new_list(name_sym, new_list(a_sym, new_list(func_sym, new_cons(func_sym, sth_sym), sth_sym), apply_sym)));
