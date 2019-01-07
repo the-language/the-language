1cd2c830ea006c6feb7efa571e0ec1ef41474490
zaoqi <zaomir@outlook.com>
2019-01-07 21:35:14 +0800

    .

--- lang.js
+++ lang.js
@@ -1,6 +1,6 @@
 /*
     The Language
-    Copyright (C) 2018  Zaoqi
+    Copyright (C) 2018, 2019  Zaoqi <zaomir@outlook.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published
@@ -282,7 +282,7 @@ var TheLanguage=(function(){
     var builtin_func_cons_head_sym=new_data(sys_sym, new_list(name_sym, new_list(a_sym, new_list(func_sym, new_list(cons_sym), sth_sym), new_symbol("首"))));
     var builtin_func_cons_tail_sym=new_data(sys_sym, new_list(name_sym, new_list(a_sym, new_list(func_sym, new_list(cons_sym), sth_sym), new_symbol("尾"))));
 
-    var builtin_func_sym_p_sym=new_data(sys_sym, new_list(name_sym, new_list(a_sym, WIP, WIP)));
+    var builtin_func_sym_p_sym=new_data(sys_sym, new_list(name_sym, new_list(a_sym, WIP, new_list(isornot_sym, new_list(a_sym, sym_sym)))));
     
     var builtin_func_equal_sym=new_data(sys_sym, new_list(name_sym, new_list(a_sym, func_sym, new_list(isornot_sym, equal_sym))));
     var builtin_form_quote_sym=new_data(sys_sym, new_list(name_sym, new_list(a_sym, form_sym, quote_sym)));
