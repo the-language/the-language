89bf29801c9669d69c822cfdde183b0bfa497fff
zaoqi <zaomir@outlook.com>
2019-01-08 21:26:13 +0800

    .

--- lang.js
+++ lang.js
@@ -277,12 +277,14 @@ var TheLanguage=(function(){
     var error_sym=new_symbol("誤");
     var sym_sym=new_symbol("符");
 
+    var builtin_func_new_data_sym=new_data(sys_sym, new_list(name_sym, new_list(a_sym, new_list(func_sym, sth_sym, data_sym), the_sym)));
+    
     var builtin_func_new_cons_sym=new_data(sys_sym, new_list(name_sym, new_list(a_sym, new_list(func_sym, sth_sym, cons_sym), the_sym)));
     var builtin_func_cons_p_sym=new_data(sys_sym, new_list(name_sym, new_list(a_sym, func_sym, new_list(isornot_sym, new_list(a_sym, cons_sym)))));
     var builtin_func_cons_head_sym=new_data(sys_sym, new_list(name_sym, new_list(a_sym, new_list(func_sym, new_list(cons_sym), sth_sym), new_symbol("首"))));
     var builtin_func_cons_tail_sym=new_data(sys_sym, new_list(name_sym, new_list(a_sym, new_list(func_sym, new_list(cons_sym), sth_sym), new_symbol("尾"))));
 
-    var builtin_func_sym_p_sym=new_data(sys_sym, new_list(name_sym, new_list(a_sym, WIP, new_list(isornot_sym, new_list(a_sym, sym_sym)))));
+    var builtin_func_sym_p_sym=new_data(sys_sym, new_list(name_sym, new_list(a_sym, func_sym, new_list(isornot_sym, new_list(a_sym, sym_sym)))));
     
     var builtin_func_equal_sym=new_data(sys_sym, new_list(name_sym, new_list(a_sym, func_sym, new_list(isornot_sym, equal_sym))));
     var builtin_form_quote_sym=new_data(sys_sym, new_list(name_sym, new_list(a_sym, form_sym, quote_sym)));
