1a089e33b6c8ba9947f05fabe84f1d7e4dfb9276
zaoqi <zaomir@outlook.com>
2019-02-07 19:25:08 +0800

    .

--- core/lang.js
+++ core/lang.js
@@ -16,6 +16,7 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 */
+'use strict';
 var TheLanguage = (function() {
     var enable_nodejs = true;
     var in_nodejs = module ? true : false; /* fix me? */
--- core/makecode.js
+++ core/makecode.js
@@ -16,6 +16,7 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 */
+'use strict';
 const L = require('./lang')
 const _E = module.exports
 
