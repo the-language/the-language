07684aa894a650fb3db7a6956f69d630f0c3e271
zaoqi <zaomir@outlook.com>
2019-02-16 21:16:09 +0800

    core/lib/

---
+++ core/lib/transform.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+transform_helper(){
+    if [ $# -gt 0 ]
+    then
+        local x="$1"
+        shift
+        sed "s$(echo -e '\0001')\*($x)$(echo -e '\0001')$(cat $x)$(echo -e '\0001')g" | transform_helper "$@"
+    else
+        cat
+    fi
+}
+transform(){
+    echo "$1" | transform_helper *
+}
+mkdir -p transformed
+cd src
+for f in *
+do
+    transform "$(cat $f)" > ../transformed/$f
+done
+
+cd ../transformed
+prog=$(cat << "EOF"
+const L = require('lang')
+const fs = require('fs')
+for(const file of fs.readdirSync('.')) {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) throw err;
+        fs.writeFile(file, L.print(L.complex_parse(data)) + '\n', (err) => {
+            if (err) throw err;
+        })
+    })
+}
+EOF
+)
+NODE_PATH=../../ecmascript ../../ecmascript/node -e "$prog"
