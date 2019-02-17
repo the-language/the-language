00bf237407591229e0ca194f6a4d0791ec2d494c
zaoqi <zaomir@outlook.com>
2019-02-17 19:14:37 +0800

    [WIP]typescript

--- core/ecmascript/compile.sh
+++ core/ecmascript/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-tsc --allowJs --outFile lang.js lang.ts
+tsc --allowJs --outFile lang.js lang.ts --types ~/.config/yarn/global/node_modules/@types/node/
 # new="$(google-closure-compiler -W QUIET --language_out ECMASCRIPT3 --js lang.js 2>/dev/null)"
 # echo "$new" > lang.js
