6c225c8c2a8ced3480d295991c514c12dfdd66a3
zaoqi <zaomir@outlook.com>
2019-03-08 21:22:22 +0800

    .

--- core/arch/lua/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-npx tstl -p tsconfig.json
-sed -i 's|local exports = exports or {};|local exports = {};|' lang.lua
--- core/arch/typescript/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd ..
-exec racket make.rkt ecmascript/lang.js
