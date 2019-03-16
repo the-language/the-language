123f78b4c90d5599e32b163257695497ebb38c60
zaoqi <zaomir@outlook.com>
2019-03-16 17:13:36 +0800

    .

--- core/arch/publish.rkt
+++ core/arch/publish.rkt
@@ -3,24 +3,27 @@
 (require rash/demo/setup)
 (require racket)
 
-(define new-version (make-parameter #f))
+(define p-new-version (make-parameter #f))
+(define p-message (make-parameter #f))
 (command-line
     #:program "publisher"
     #:once-each
-    [("--new-version") version "new version" (new-version version)]
+    [("--new-version") version "new version" (p-new-version version)]
+    [("--message") message "message" (p-message message)]
     #:args ()
     {
+        (unless (p-new-version) (raise "ERROR!"))
+        (unless (p-message) (p-message ""))
+
         racket make.rkt
 
         in-dir "ecmascript" {
-            yarn publish --no-git-tag-version --no-commit-hooks --new-version (new-version)
+            yarn publish --no-git-tag-version --no-commit-hooks --new-version (p-new-version) --message (p-message)
         }
 
-
         git add .
-        git commit -m (new-version)
-        git tag -a (new-version) -m (new-version)
-        git push
-        git push --tags
+        git commit -m (string-append "[NEW VERSION]"(p-new-version)"\n"(p-message))
+        git tag -a (p-new-version) -m (p-message)
+        git push --tags --all
     }
 )
