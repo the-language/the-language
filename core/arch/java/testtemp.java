d926c885765109368e63e27d7a535c2e5bfa0c90
zaoqi <zaomir@outlook.com>
2019-03-17 16:32:38 +0800

    [WIP] java

--- core/arch/java/testtemp.java
+++ core/arch/java/testtemp.java
@@ -3,28 +3,12 @@ import org.luaj.vm2.lib.jse.*;
 import java.util.Vector;
 
 public class testtemp {
-
-  private static Globals globals;
-  private static boolean print = false;
-  private static String encoding = null;
-
   public static void main(String[] args) throws Exception {
-
-    // process args
-    boolean interactive = (args.length == 0);
-    boolean versioninfo = false;
-    boolean processing = true;
-    boolean nodebug = false;
-    boolean luajc = false;
-    Vector libs = null;
-
-    // new lua state
-    globals = JsePlatform.standardGlobals();
+    Globals globals = JsePlatform.standardGlobals();
     String libname = "lang$luaj";
     LuaValue slibname = LuaValue.valueOf(libname);
-    // load as java class
-    LuaValue v = (LuaValue)Class.forName(libname).newInstance();
-    v.call(slibname, globals);
+    LuaValue v = (LuaValue)(new lang$luaj());
+    LuaValue exports = v.call(slibname, globals);
     System.out.println("load as java class");
   }
 }
