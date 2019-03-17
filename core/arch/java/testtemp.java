bdc42d60c847845c4b6c32d54d3958094aa32dfd
zaoqi <zaomir@outlook.com>
2019-03-17 16:41:12 +0800

    [WIP] java

--- core/arch/java/testtemp.java
+++ core/arch/java/testtemp.java
@@ -1,14 +1,21 @@
 import org.luaj.vm2.*;
 import org.luaj.vm2.lib.jse.*;
-import java.util.Vector;
 
 public class testtemp {
+  private static Globals globals;
+  private static LuaTable exports;
+  private static boolean inited = false;
+  private static void init() throws Exception {
+    if (!inited) {
+      globals = JsePlatform.standardGlobals();
+      exports = (LuaTable)((LuaValue)(new lang$luaj()))
+                    .call(LuaValue.valueOf("lang$luaj"), globals);
+      inited = true;
+    }
+  }
   public static void main(String[] args) throws Exception {
-    Globals globals = JsePlatform.standardGlobals();
-    String libname = "lang$luaj";
-    LuaValue slibname = LuaValue.valueOf(libname);
-    LuaValue v = (LuaValue)(new lang$luaj());
-    LuaValue exports = v.call(slibname, globals);
-    System.out.println("load as java class");
+    init();
+    System.out.println("load as java class|" +
+                       exports.get("complex_parse").toString());
   }
 }
