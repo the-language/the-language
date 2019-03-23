cfccbf88377b9c69795113dc008713fc2a3e2611
zaoqi <zaomir@outlook.com>
2019-03-23 14:00:04 +0800

    [WIP] jtransc

---
+++ core/arch/jtransc/src/Main.java
@@ -0,0 +1,6 @@
+import lang.*;
+public class Main {
+        static public void main(String[] args) throws Exception {
+                System.out.println(Library.someLibraryMethod(Lang.simple_print(Lang.complex_parse(":&>化滅"))));
+        }
+}
