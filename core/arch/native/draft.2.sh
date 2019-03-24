cc0071aaf725f5aaa6c69f5db11d09ccb127c794
zaoqi <zaomir@outlook.com>
2019-03-24 18:26:10 +0800

    [WIP] native

--- core/arch/native/draft.2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-for arch in x86_64-linux-gnu ;do
-    ${arch}-gcj -O3 -S $(find ../java/src/ -name '*.java') -o a.${arch}.s
-done
--- core/arch/native/draft.sh
+++ core/arch/native/draft.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 for arch in x86_64-linux-gnu ;do
     ${arch}-gcj -O3 --main=testmain $(find ../java/src/ -name '*.java') -o a.out.${arch}
+    ${arch}-gcj -O3 -S $(find ../java/src/ -name '*.java') -o a.${arch}.s
+    ${arch}-gcj -O3 -c $(find ../java/src/ -name '*.java') -o a.${arch}.o
+     ~/src/retdec/build/bin/retdec-decompiler.py a.${arch}.o
 done
