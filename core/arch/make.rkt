cb89be8c1d266cc302285bde7db2b9915daf1fff
zaoqi <zaomir@outlook.com>
2019-03-30 22:34:23 +0800

    C : single file

--- core/arch/make.rkt
+++ core/arch/make.rkt
@@ -177,11 +177,8 @@
              |> id raw &>! lang.lua2c.out.c
 
              (define out (++
-                 c-generatedby
-                 c-copyright
                  (match (string->lines raw) [(list-rest head-list ... '("static int lc_pmain(lua_State *L) {" "  luaL_openlibs(L);" "" "  const lc_args_t *const args = (lc_args_t *)lua_touserdata(L, 1);" "  lc_createarg(L, args);" "" "  lua_pushcfunction(L, traceback);" "" "  const int status1 = lc_handle_luainit(L);" "  if (status1 != 0)" "    return 0;" "" "  /* note: IMPROVE: closure not always needed here */" "  lua_newtable(L); /* closure table */" "  lua_pushcclosure(L, lcf_main, 1);" "  int i;" "  for (i = 1; i < args->c; i++) {" "    lua_pushstring(L, args->v[i]);" "  }" "  int status2 = lua_pcall(L, args->c - 1, 0, -2);" "  if (status2 != 0) {" "    const char *msg = lua_tostring(L, -1);" "    if (msg == NULL)" "      msg = \"(error object is not a string)\";" "    fputs(msg, stderr);" "  }" "  return 0;" "}" "" "int main(int argc, const char **argv) {" "  lc_args_t args = {argc, argv};" "  lua_State *L = luaL_newstate();" "  if (!L) {" "    fputs(\"Failed creating Lua state.\", stderr);" "    exit(1);" "  }" "" "  int status = lua_cpcall(L, lc_pmain, &args);" "  if (status != 0) {" "    fputs(lua_tostring(L, -1), stderr);" "  }" "" "  lua_close(L);" "  return 0;" "}")) (lines->string head-list)])
-                 #{cat ./patch/lang.tail.c}
-                 ))
+                 #{cat ./patch/lang.tail.c}))
 
              mkdir -p src
              rm -fr src
@@ -195,7 +192,11 @@
              rm ./src/loslib.c ./src/lmathlib.c ./src/ltablib.c ./src/liolib.c ./src/ldblib.c ./src/loadlib.c
 
              ./gen.single.sh
-             touch lang.c
+             (define single (++
+                   c-generatedby
+                   c-copyright
+                   #{cat lang.c}))
+             |> id single &>! lang.c
              touch lang.h
 
              clang -O3 -Oz -o testmain testmain.c lang.c -lm
