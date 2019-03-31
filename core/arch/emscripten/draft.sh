92164a52082c839522560bd9f970598e9e7368ad
zaoqi <zaomir@outlook.com>
2019-03-31 19:04:37 +0800

    [WIP] emscripten

--- core/arch/emscripten/draft.sh
+++ core/arch/emscripten/draft.sh
@@ -7,10 +7,10 @@
 sh -c (id "[ -d emsdk ] || git clone --depth 1 https://github.com/emscripten-core/emsdk.git")
 
 in-dir "emsdk" {
+  mkdir -p zips
   in-dir "zips" {
-    sh -c (id "{ [ -f emscripten-llvm-e1.38.30.tar.gz ]&&[ ! -f emscripten-llvm-e1.38.30.tar.gz.aria2 ];} || aria2c -s 10 -j 10 -x 10 -k 1M https://s3.amazonaws.com/mozilla-games/emscripten/packages/llvm/tag/linux_64bit/emscripten-llvm-e1.38.30.tar.gz")
+    sh -c (id "[ -d ../clang/e1.38.30_64bit ] || { [ -f emscripten-llvm-e1.38.30.tar.gz ]&&[ ! -f emscripten-llvm-e1.38.30.tar.gz.aria2 ];} || aria2c -s 10 -j 10 -x 10 -k 1M https://s3.amazonaws.com/mozilla-games/emscripten/packages/llvm/tag/linux_64bit/emscripten-llvm-e1.38.30.tar.gz")
   }
   ./emsdk install latest
   ./emsdk activate latest
-  source ./emsdk_env.sh
 }
