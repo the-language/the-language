3815e7770fd27962707f8a77f1c96213e67c1c29
zaoqi <zaomir@outlook.com>
2019-03-31 19:33:49 +0800

    [WIP] emscripten

--- core/arch/emscripten/draft.sh
+++ core/arch/emscripten/draft.sh
@@ -1,16 +1,14 @@
-#lang rash
-;; in-dir is in the demo file still
-(require rash/demo/setup)
+#!/bin/bash
+[ -d emsdk ] || git clone --depth 1 https://github.com/emscripten-core/emsdk.git
 
-(define (id x) x)
-
-sh -c (id "[ -d emsdk ] || git clone --depth 1 https://github.com/emscripten-core/emsdk.git")
-
-in-dir "emsdk" {
+pushd "emsdk"
   mkdir -p zips
-  in-dir "zips" {
-    sh -c (id "[ -d ../clang/e1.38.30_64bit ] || { [ -f emscripten-llvm-e1.38.30.tar.gz ]&&[ ! -f emscripten-llvm-e1.38.30.tar.gz.aria2 ];} || aria2c -s 10 -j 10 -x 10 -k 1M https://s3.amazonaws.com/mozilla-games/emscripten/packages/llvm/tag/linux_64bit/emscripten-llvm-e1.38.30.tar.gz")
-  }
+  pushd "zips"
+    [ -d ../clang/e1.38.30_64bit ] || { [ -f emscripten-llvm-e1.38.30.tar.gz ]&&[ ! -f emscripten-llvm-e1.38.30.tar.gz.aria2 ];} || aria2c -s 10 -j 10 -x 10 -k 1M https://s3.amazonaws.com/mozilla-games/emscripten/packages/llvm/tag/linux_64bit/emscripten-llvm-e1.38.30.tar.gz
+  popd
   ./emsdk install latest
   ./emsdk activate latest
-}
+popd
+. ./emsdk/emsdk_env.sh
+emcc -O3 -Oz ../c/lang.c ../c/testmain.c -o lang.js
+./emsdk/clang/e1.38.30_64bit/binaryen/bin/wasm-dis lang.wasm > lang.wat
