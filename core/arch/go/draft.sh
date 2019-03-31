6faeee52c3538c44c8f9a9b7b881074621a9bdc7
zaoqi <zaomir@outlook.com>
2019-03-31 13:52:21 +0800

    [WIP] Go

--- core/arch/go/draft.sh
+++ core/arch/go/draft.sh
@@ -5,5 +5,5 @@ mkdir -p ./deps/src/golang.org/x/
 export GOPATH="$PWD/deps"
 [ -d ./deps/src/golang.org/x/tools ] || (pushd ./deps/src/golang.org/x && git clone --depth 1 https://github.com/golang/tools.git && popd)
 [ -d ./deps/src/github.com/elliotchance/c2go/ ] || (pushd ./deps/src/github.com && git clone --depth 1 https://github.com/elliotchance/c2go.git && popd)
-go get github.com/elliotchance/c2go
+[ -f ./deps/bin/c2go ] || go get github.com/elliotchance/c2go
 ./deps/bin/c2go transpile ../c/lang.c
