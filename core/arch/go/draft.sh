7ade276ad241c7fdff0163cf41295e03e869e7a5
zaoqi <zaomir@outlook.com>
2019-04-04 19:55:08 +0800

    .

--- core/arch/go/draft.sh
+++ core/arch/go/draft.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#deps: golang 1.9+
+#deps: golang 1.10+
 mkdir -p deps
-mkdir -p ./deps/src/golang.org/x/
+#mkdir -p ./deps/src/golang.org/x/
 export GOPATH="$PWD/deps"
-[ -d ./deps/src/golang.org/x/tools ] || (pushd ./deps/src/golang.org/x && git clone --depth 1 https://github.com/golang/tools.git && popd)
-[ -d ./deps/src/github.com/elliotchance/c2go/ ] || (pushd ./deps/src/github.com && git clone --depth 1 https://github.com/elliotchance/c2go.git && popd)
-[ -f ./deps/bin/c2go ] || go get github.com/elliotchance/c2go
-./deps/bin/c2go transpile ../c/lang.c
+#[ -d ./deps/src/golang.org/x/tools ] || (pushd ./deps/src/golang.org/x && git clone --depth 1 https://github.com/golang/tools.git && popd)
+[ -d ./deps/src/github.com/Konstantin8105/c4go/ ] || (mkdir -p ./deps/src/github.com/Konstantin8105 && pushd ./deps/src/github.com/Konstantin8105 && git clone --depth 1 https://github.com/Konstantin8105/c4go.git && popd)
+[ -f ./deps/bin/c4go ] || go get github.com/Konstantin8105/c4go
+#./deps/bin/c4go transpile ../c/lang.c
