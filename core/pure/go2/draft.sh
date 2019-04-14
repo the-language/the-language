49c79484f4ded732231b2f3cb796be5142185b0e
zaoqi <zaomir@outlook.com>
2019-04-14 11:36:34 +0800

    [WIP] Go2

--- core/pure/go2/draft.sh
+++ core/pure/go2/draft.sh
@@ -4,4 +4,4 @@ mkdir -p deps
 export GOPATH="$PWD/deps"
 [ -d ./deps/src/github.com/Konstantin8105/c4go/ ] || (mkdir -p ./deps/src/github.com/Konstantin8105 && pushd ./deps/src/github.com/Konstantin8105 && git clone --depth 1 https://github.com/Konstantin8105/c4go.git && popd)
 [ -f ./deps/bin/c4go ] || go get github.com/Konstantin8105/c4go
-./deps/bin/c4go transpile ../c/lang.c ../c/testmain.c
+./deps/bin/c4go transpile -s ../c/lang.c ../c/testmain.c
