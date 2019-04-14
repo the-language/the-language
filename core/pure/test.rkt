144ad3dd8f1f9def45155e01a5dd5f27c54471c1
zaoqi <zaomir@outlook.com>
2019-04-14 14:46:59 +0800

    test

--- core/pure/test.rkt
+++ core/pure/test.rkt
@@ -53,7 +53,7 @@
      (++ "console.log(\""(make-safe-string/double-quote (with-output-to-string (λ () (write code))))"\");\nassert.strictEqual("(js-expr-compile v1)","(js-expr-compile v2)");\n")]
     [(list 'begin xs ...)
      (apply ++ (map js-test-compile xs))]))
-(define (js-run x) (system (string-append "node -e 'const L=require(\"./ecmascript/lang.js\");"(assert-safe-string/single-quote x)"'")))
+(define (js-run x) (system (string-append "node -e 'const L=require(\"./ecmascript/lang.js\");\n"(assert-safe-string/single-quote x)"'")))
 
 (define lisp-id->py3-id lisp-id->js-id)
 (define (py3-expr-compile x)
@@ -70,6 +70,23 @@
     [(list 'begin xs ...)
      (apply ++ (map py3-test-compile xs))]))
 (define (py3-run x) (system (string-append "echo 'import python3.lang as L\n"(assert-safe-string/single-quote x)"' | python3")))
+
+(define (lisp-id->lua-id x)
+  (apply-++ (map (λ (c) (assert-value char? c) (if (equal? c #\-) "_" (string c))) (string->list (symbol->string x)))))
+(define (lua-expr-compile x)
+  (match x
+    [(? string?) (with-output-to-string (λ () (write x)))]
+    [(? number?) (number->string x)]
+    [(list (? string? f) xs ...) (++ "L."f"("(apply-++ (add-between (map (λ (x) (lua-expr-compile x)) xs) ","))")")]
+    [(list (? symbol? f) xs ...) (lua-expr-compile (cons (lisp-id->lua-id f) xs))]
+    ))
+(define (lua-test-compile code)
+  (match code
+    [(list 'check-equal? v1 v2)
+     (++ "print(\""(make-safe-string/double-quote (with-output-to-string (λ () (write code))))"\")\nassert("(lua-expr-compile v1)" == "(lua-expr-compile v2)")\n")]
+    [(list 'begin xs ...)
+     (apply ++ (map lua-test-compile xs))]))
+(define (lua-run x) (system (string-append "./c/lua-5.1.5/src/lua -e 'local L=require(\"./lua/lang\")\n"(assert-safe-string/single-quote x)"'")))
 
 (define test-main
   `(begin
@@ -155,6 +172,9 @@
  (begin
    (displayln "-- JS --")
    (js-run (js-test-compile test-main)))
+ (begin
+   (displayln "-- Lua --")
+   (lua-run (lua-test-compile test-main)))
  (begin
    (displayln "-- PY3 --")
    (py3-run (py3-test-compile test-main)))))
