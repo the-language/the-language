c51a8d5091b0d0bcccc734f0f320eb71202b8349
zaoqi <zaomir@outlook.com>
2019-05-06 03:45:42 +0000

    .

--- core/pure/java/compile-luaj.sh
+++ core/pure/java/compile-luaj.sh
@@ -2,7 +2,7 @@
 
 ./get-ant.sh
 
-[ -d luaj ] || git clone --depth 1 https://github.com/luaj/luaj.git ||exit
+[ -d luaj ] || git clone --depth 1 https://gitlab.com/the-language/luaj.git ||exit
 cd luaj
 [ -f luaj-jse-3.0.2.jar ] || ../ant/bin/ant ||exit
 cd -
