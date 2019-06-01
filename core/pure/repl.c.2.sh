ed932e7df5cb77b3d7407dab73a0a3ae5bdcdbb8
zaoqi <zaomir@outlook.com>
2019-06-02 01:57:05 +0800

    .

--- core/pure/repl.c.2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-[ -f "$(dirname "$0")"/node_modules/.bin/ya-igcc ] || yarn
-LD_LIBRARY_PATH="$(dirname "$0")"/c "$(dirname "$0")"/node_modules/.bin/ya-igcc -Pc -I"$(dirname "$0")"/c -Pc -L"$(dirname "$0")"/c -Pc -llang -e ':include lang.h' "$@"
--- core/pure/repl.c.sh
+++ core/pure/repl.c.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-[ -f "$(dirname "$0")"/node_modules/.bin/igcc ] || yarn
-echo '請輸入#include <lang.h>'
-LD_LIBRARY_PATH="$(dirname "$0")"/c "$(dirname "$0")"/node_modules/.bin/igcc -I "$(dirname "$0")"/c -L "$(dirname "$0")"/c -llang "$@"
+[ -f "$(dirname "$0")"/node_modules/.bin/ya-igcc ] || yarn
+LD_LIBRARY_PATH="$(dirname "$0")"/c "$(dirname "$0")"/node_modules/.bin/ya-igcc -Pc -I"$(dirname "$0")"/c -Pc -L"$(dirname "$0")"/c -Pc -llang -e ':include lang.h' "$@"
