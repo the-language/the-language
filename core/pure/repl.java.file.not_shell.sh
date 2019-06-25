fc4c1c4a3657e3ded9a854c951f714b675ddeace
zaoqi <zaomir@outlook.com>
2019-06-25 14:12:09 +0000

    .

--- core/pure/repl.java.file.not_shell.sh
+++ core/pure/repl.java.file.not_shell.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+oldpwd="$(pwd)"
+cd "$(dirname "$0")" ||exit
+bin="$(pwd)"
+make java/src ||exit
 d="$(mktemp -d)"
 f="$d/Main.java"
-
 echo 'import lang.*;' > "$f"
 echo 'public class Main {' >> "$f"
 echo 'public static void main(String[] args) throws Exception {' >> "$f"
 cat "$1" >> "$f"
 echo '}}' >> "$f"
-
-pushd "$(dirname "$0")/java/src"
-java_src="$(pwd)"
-popd
-
-pushd "$d"
-export CLASSPATH="$java_src:."
+cd "$d" ||exit
+export CLASSPATH="$bin/java/src:$d"
 javac Main.java ||exit
+cd "$oldpwd" ||exit
 java Main ||exit
-popd
+s="$?"
+rm "$d"
+exit "$s"
