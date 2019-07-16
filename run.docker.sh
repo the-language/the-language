859a7bf99d498a25e6a7e62099968ca09b977aa9
zaoqi <zaomir@outlook.com>
2019-07-16 18:38:12 +0800

    .

--- run.docker.sh
+++ run.docker.sh
@@ -2,4 +2,13 @@
 set -e
 img_name=the-language-builder
 docker build -t "$img_name" .
-exec docker run --rm -it -v "$(pwd):$(pwd)" "$img_name" /bin/sh -c "groupadd -g '$(id -g)' '$(id -ng)'&&useradd -MN -g '$(id -g)' -u '$(id -u)' '$(id -nu)'&&echo '$(id -nu) ALL=(ALL) NOPASSWD: ALL'>>/etc/sudoers&&mkdir -p '$HOME'&&chown -R '$(id -u):$(id -g)' '$HOME'&&cd '$(pwd)'&&su '$(id -nu)'"
+exec docker run --rm -it -v "$(pwd):$(pwd)" "$img_name" /bin/sh -c "
+  groupadd -g '$(id -g)' '$(id -ng)'&&
+  mkdir -p '$HOME'&&
+  cp -Tr /etc/skel '$HOME'&&
+  chown -R '$(id -u):$(id -g)' '$HOME'&&
+  useradd -MN -d '$HOME' -g '$(id -g)' -u '$(id -u)' '$(id -nu)'&&
+  echo '$(id -nu) ALL=(ALL) NOPASSWD: ALL'>>/etc/sudoers&&
+  cd '$(pwd)'&&
+  su '$(id -nu)'
+"
