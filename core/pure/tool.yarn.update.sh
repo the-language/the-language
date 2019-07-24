55865d6703d930e9546de835c35c14a8d313b9c1
zaoqi <zaomir@outlook.com>
2019-07-24 21:47:44 +0800

    [WIP]修复自动更新脚本

--- core/pure/tool.yarn.update.sh
+++ core/pure/tool.yarn.update.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
+
+set -e
+
 up(){
+  rm -fr node_modules yarn.lock
+  npm install
+  npm update
+  rm -fr node_modules package-lock.json
   yarn
-  yarn upgrade $(node -e 'pkg=JSON.parse(fs.readFileSync("package.json","utf8"));console.log(Object.keys(pkg.devDependencies||{}).concat(Object.keys(pkg.dependencies||{})).reduce((x,y)=>`${x} ${y}`))')
-  touch node_modules/
 }
 if [ -f package.json ]; then
   up &
 fi
 for d in */; do
-  cd "$d" ||exit
+  cd "$d"
   if [ -f package.json ]; then
     up &
   fi
-  cd .. ||exit
+  cd ..
 done
 
 wait
