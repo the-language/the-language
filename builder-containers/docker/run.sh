70e1ff39fdadd6c4054f21eb9db278ed971f2fd1
zaoqi <zaomir@outlook.com>
2019-08-10 00:26:45 +0800

    update builder-containers/docker

--- builder-containers/docker/run.sh
+++ builder-containers/docker/run.sh
@@ -10,18 +10,9 @@ cd ../../
 src="$(pwd)"
 cd "$bin"
 img_name=zaoqi/the-language-builder
-docker build -t "$img_name" .
+docker build --build-arg UID="$(id -u)" --build-arg GID="$(id -g)" -t "$img_name" .
 mkdir -p ../containers_cache/.cache
 cd ../containers_cache
 cache="$(pwd)"
 cd "$bin"
-exec docker run --rm -it -v "$cache/.cache:$HOME/.cache" -v "$src:$src" "$img_name" /bin/sh -c "
-  groupadd -g '$(id -g)' '$(id -ng)'&&
-  mkdir -p '$HOME'&&
-  cp -Tr /etc/skel '$HOME'&&
-  chown -R '$(id -u):$(id -g)' '$HOME'&&
-  useradd -MN -d '$HOME' -g '$(id -g)' -u '$(id -u)' '$(id -nu)'&&
-  echo '$(id -nu) ALL=(ALL) NOPASSWD: ALL'>>/etc/sudoers&&
-  cd '$src'&&
-  su '$(id -nu)'
-"
+exec docker run --rm -it -v "$cache/.cache:$HOME/.cache" -v "$src:$src" "$img_name" "$@"
